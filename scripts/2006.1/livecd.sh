b6fb39f5e592386f6c0d0884c27cb4ed07189cf0
Chris Gianelloni <wolf31o2@gentoo.org>
2006-08-11 19:34:33 +0000

    Update fsscript so it checks to make sure the files exist before using sed on them.  This allows us to use a single fsscript on all arches.
    svn path=/trunk/; revision=190

--- scripts/2006.1/livecd.sh
+++ scripts/2006.1/livecd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
- 
-sed -e '/^[^#]\+=.\+$/d' /etc/X11/gdm/custom.conf > \
- 	/etc/X11/gdm/custom.conf.old
-sed -i -e 's/gentoo-emergence/gentoo-livecd-2006.1/' \
-	/etc/X11/gdm/custom.conf
+
+if [ -e /etc/X11/gdm/custom.conf ]
+then
+	sed -e '/^[^#]\+=.\+$/d' /etc/X11/gdm/custom.conf > \
+ 		/etc/X11/gdm/custom.conf.old
+	sed -i -e 's/gentoo-emergence/gentoo-livecd-2006.1/' \
+		/etc/X11/gdm/custom.conf
+fi
