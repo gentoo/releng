2d5c5bebd7f13fdb2d462964f36d6792a88334b5
Chris Gianelloni <wolf31o2@gentoo.org>
2006-08-17 19:20:36 +0000

    Updated to set the background the correct way.  Thanks to the guys at the GNOME Foundation booth at LWE for the hint.
    svn path=/trunk/; revision=192

--- scripts/2006.1/livecd.sh
+++ scripts/2006.1/livecd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+ 
 if [ -e /etc/X11/gdm/custom.conf ]
 then
 	sed -e '/^[^#]\+=.\+$/d' /etc/X11/gdm/custom.conf > \
- 		/etc/X11/gdm/custom.conf.old
+ 	/etc/X11/gdm/custom.conf.old
 	sed -i -e 's/gentoo-emergence/gentoo-livecd-2006.1/' \
-		/etc/X11/gdm/custom.conf
+	/etc/X11/gdm/custom.conf
 fi
+
+gconftool-2 --direct \
+	--config-source xml:readwrite:/usr/livecd/gconf/gconf.xml.defaults \
+	--type string --set /desktop/gnome/background/picture_filename \
+	/usr/share/pixmaps/gentoo-livecd-2006.1/gentoo-livecd-2006.1-1024x768.png
