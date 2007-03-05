d1f9bd6d31beb1bff203636a33df40eff5173319
Chris Gianelloni <wolf31o2@gentoo.org>
2007-03-05 23:39:48 +0000

    Added an initial fsscript for 2007.0, which won't work until we have a theme set.
    svn path=/trunk/; revision=260

---
+++ scripts/2007.0/livecd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+if [ -e /etc/X11/gdm/custom.conf ]
+then
+	sed -e '/^[^#]\+=.\+$/d' /etc/X11/gdm/custom.conf > \
+ 	/etc/X11/gdm/custom.conf.old
+	sed -i -e 's/gentoo-emergence/gentoo-livecd-2007.0/' \
+	/etc/X11/gdm/custom.conf
+fi
+
+if [ -e /etc/X11/gdm/gdm.conf ]
+then
+	sed -i -e 's/gentoo-emergence/gentoo-livecd-2007.0/' \
+	/etc/X11/gdm/gdm.conf
+fi
+
+gconftool-2 --direct \
+	--config-source xml:readwrite:/usr/livecd/gconf/gconf.xml.defaults \
+	--type string --set /desktop/gnome/background/picture_filename \
+	/usr/share/pixmaps/gentoo-livecd-2007.0/gentoo-livecd-2007.0-1024x768.png
+
+case `uname -m` in
+	i?86|x86_64)
+		sed -i 's/DRIVER fbdev/DRIVER vesa/' /usr/share/hwdata/Cards
+	;;
+esac
+
