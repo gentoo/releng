10e2b30994209929f684e8f534a570d0162ec49b
Anthony G. Basile <blueness@gentoo.org>
2013-05-07 18:48:41 -0400

    tools-uclibc/desktop: add user and desktop

--- tools-uclibc/desktop/build.sh
+++ tools-uclibc/desktop/build.sh
@@ -1,3 +1,4 @@
+# These are just notes for now.  A cleaner script is coming.
 
 mkdir desktop-amd64-uclibc-hardened
 tar -x -C desktop-amd64-uclibc-hardened -f /var/tmp/catalyst/builds/hardened/amd64/stage3-amd64-uclibc-hardened.tar.bz2
@@ -14,6 +15,8 @@ mkdir desktop-amd64-uclibc-hardened/var/lib/layman
 cp -a /var/lib/layman/hardened-development/ desktop-amd64-uclibc-hardened/var/lib/layman
 
 cp -f fstab desktop-amd64-uclibc-hardened/etc/fstab 
+cp -f lilo.conf desktop-amd64-uclibc-hardened/etc/lilo.conf
+
 rm -f desktop-amd64-uclibc-hardened/etc/portage/make.conf.catalyst
 cp -f portage/make.conf.1 desktop-amd64-uclibc-hardened/etc/portage/make.conf
 
@@ -51,12 +54,41 @@ genkernel \
 	--modprobedir=/root/lilblue/desktop-amd64-uclibc-hardened/etc/modprobe.d \
 	all
 
+ln -sf net.lo desktop-amd64-uclibc-hardened/etc/init.d/net.eth0
+chroot desktop-amd64-uclibc-hardened/ rc-update add alsasound default
+chroot desktop-amd64-uclibc-hardened/ rc-update add cupsd default
+chroot desktop-amd64-uclibc-hardened/ rc-update add fcron default
+chroot desktop-amd64-uclibc-hardened/ rc-update add net.eth0 default
+chroot desktop-amd64-uclibc-hardened/ rc-update add postfix default
+chroot desktop-amd64-uclibc-hardened/ rc-update add sshd default
+chroot desktop-amd64-uclibc-hardened/ rc-update add xdm default
+chroot desktop-amd64-uclibc-hardened/ rc-update add avahi-daemon default
+chroot desktop-amd64-uclibc-hardened/ rc-update add dbus default
+chroot desktop-amd64-uclibc-hardened/ rc-update add samba default
+chroot desktop-amd64-uclibc-hardened/ rc-update add syslog-ng default
+
+cp -f passwd.sh desktop-amd64-uclibc-hardened/tmp/
+chroot  desktop-amd64-uclibc-hardened/ /tmp/passwd.sh
+rm -f desktop-amd64-uclibc-hardened/tmp/passwd.sh
+
+rm -rf desktop-amd64-uclibc-hardened/home/gentoo
+cp -a gentoo desktop-amd64-uclibc-hardened/home/
+
+sed -i 's/^\(DISPLAYMANAGER="\)xdm/\1slim/' desktop-amd64-uclibc-hardened/etc/conf.d/xdm
+sed -i 's/^\(login.*\)/# \1/' desktop-amd64-uclibc-hardened/etc/slim.conf
+sed -i '/# login_cmd.*Xsession/ a\login_cmd exec /bin/bash -login ~/.xinitrc' desktop-amd64-uclibc-hardened/etc/slim.conf
+wget -O desktop-amd64-uclibc-hardened/usr/share/slim/themes/default/background.jpg http://www.gentoo.org/images/backgrounds/gentoo1600x1200.jpg
+
+sed -i '/^SYNC/d' desktop-amd64-uclibc-hardened/etc/portage/make.conf
+sed -i '/^GENTOO_MIRRORS/d' desktop-amd64-uclibc-hardened/etc/portage/make.conf
+sed -i 's/^MAKEOPTS/#MAKEOPTS/' desktop-amd64-uclibc-hardened/etc/portage/make.conf
+
+rm -rf desktop-amd64-uclibc-hardened/tmp/*
+rm -rf desktop-amd64-uclibc-hardened/var/log/*
+rm -rf desktop-amd64-uclibc-hardened/var/tmp/*
+
 umount desktop-amd64-uclibc-hardened/sys/
 umount desktop-amd64-uclibc-hardened/dev/pts/
 umount desktop-amd64-uclibc-hardened/dev/
 umount desktop-amd64-uclibc-hardened/proc/
 umount desktop-amd64-uclibc-hardened/usr/portage/
-
-sed -i '/^SYNC/d' desktop-amd64-uclibc-hardened/etc/portage/make.conf
-sed -i '/^GENTOO_MIRRORS/d' desktop-amd64-uclibc-hardened/etc/portage/make.conf
-sed -i 's/^MAKEOPTS/#MAKEOPTS/' desktop-amd64-uclibc-hardened/etc/portage/make.conf
