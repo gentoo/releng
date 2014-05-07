b4c1ef5c2ba803701cdd1eff4a8894abeaab98a1
Devan Franchini <twitch153@gentoo.org>
2014-05-07 16:56:36 -0400

    tools-hardened/desktop: Adds ability to set wallpaper for fluxbox.
    Previously, if you were to boot this image you would be greeted with
    an error message telling you that the background could not be set.
    To fix this the package eterm was added to the fluxbox world file
    to be pulled in on build-time. However, this only solved the problem
    of being unable to set the background manually. To have fluxbox set
    it automatically the .xinit was altered to call "exec startfluxbox".
    When fluxbox is called by that command it searches for and executes
    the startup file located in ~/.fluxbox/. This commit also adds a
    custom startup file which sets the background to the default background:
    /usr/share/backgrounds/background.jpg. This commit also includes one
    unrelated alteration to fluxbox-run.sh which changes the fluxbox init
    file to look at the custom menu in /usr/share/fluxbox/usermenu instead
    of ~/.fluxbox/my-menu.

--- tools-hardened/desktop/fluxbox-run.sh
+++ tools-hardened/desktop/fluxbox-run.sh
@@ -26,18 +26,22 @@ setup_usergroups() {
 	cp -a thuser "${ROOTFS}"/etc/skel
 
 	cp -f files/usermenu "${ROOTFS}"/usr/share/fluxbox/
-	sed -i 's/^\/usr\/*.*/\/usr\/bin\/fluxbox/' "${ROOTFS}"/etc/skel/.xinitrc
-	mkdir -p "${ROOTFS}"/etc/skel/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos,.ssh,.cache/dconf,.config/dconf}
+	cp -f files/fluxbox-startup "${ROOTFS}"/usr/share/fluxbox/startup
+
+	sed -i 's/^\/usr\/*.*/exec startfluxbox/' "${ROOTFS}"/etc/skel/.xinitrc
+	mkdir -p "${ROOTFS}"/etc/skel/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos,.ssh,.cache/dconf,.config/dconf,.fluxbox}
+
 	chmod 700 "${ROOTFS}"/etc/skel/.ssh
 	wget -O "${ROOTFS}"/etc/skel/.config/dconf/user "${DCONF_LOCAL}"
 	wget -O "${ROOTFS}"/etc/skel/.cache/dconf/user "${DCONF_LOCAL}"
 
 	rm -rf "${ROOTFS}"/home/thuser
 	cp -a thuser "${ROOTFS}"/home/thuser
-	sed -i -e 's/^\/usr\/*.*/\/usr\/bin\/fluxbox/' "${ROOTFS}"/home/thuser/.xinitrc
+	sed -i -e 's/^\/usr\/*.*/exec startfluxbox/' "${ROOTFS}"/home/thuser/.xinitrc
 	cp -a files/{Encrypt,Save,Utilities} "${ROOTFS}"/home/thuser
 	rm -rf "${ROOTFS}"/home/thuser/Utilities/post_gnome3_install.sh
-	mkdir -p "${ROOTFS}"/home/thuser/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos,.ssh,.cache/dconf,.config/dconf}
+	mkdir -p "${ROOTFS}"/home/thuser/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos,.ssh,.cache/dconf,.config/dconf,.fluxbox}
+	cp -f "${ROOTFS}"/usr/share/fluxbox/{apps,init,keys,menu,overlay,startup,usermenu,windowmenu} /home/thuser/.fluxbox/
 	chmod 700 "${ROOTFS}"/home/thuser/.ssh
 	wget -O "${ROOTFS}"/home/thuser/.config/dconf/user "${DCONF_LOCAL}"
 	wget -O "${ROOTFS}"/home/thuser/.cache/dconf/user "${DCONF_LOCAL}"
@@ -45,8 +49,6 @@ setup_usergroups() {
 	chroot "${ROOTFS}"/ chown -R thuser:thuser /home/thuser
 	sed -i 's/# \(%wheel.*NOPASSWD\)/\1/' "${ROOTFS}"/etc/sudoers
 	sed -i 's/^\/usr\/*.*/\/usr\/bin\/fluxbox/' "${ROOTFS}"/etc/skel/.xinitrc
-	mkdir -p "${ROOTFS}"/home/thuser/.fluxbox
-	cp -f files/usermenu "${ROOTFS}"/home/thuser/.fluxbox/my-menu
 }
 
 setup_confs() {
@@ -57,7 +59,8 @@ setup_confs() {
 	sed -i '/# login_cmd.*Xsession/ a\login_cmd exec /bin/bash -login ~/.xinitrc' "${ROOTFS}"/etc/slim.conf
 	sed -i 's/^\(sessiondir.*\)/# \1/' "${ROOTFS}"/etc/slim.conf
 	sed -i '/# sessiondir.*/ a\sessiondir /etc/X11/Sessions' "${ROOTFS}"/etc/slim.conf
-	sed -i 's/^session\.menuFile.*./session\.menuFile:  \~\/.fluxbox\/my-menu/' "${ROOTFS}"/usr/share/fluxbox/init
+	sed -i 's/^session\.menuFile.*./session\.menuFile:  \/usr\/share\/fluxbox\/usermenu/' "${ROOTFS}"/usr/share/fluxbox/init
+	mkdir -p "${ROOTFS}"/usr/share/backgrounds/
 	wget -O "${ROOTFS}"/usr/share/backgrounds/background.jpg "${IMAGE}"
 
 	sed -i '/^SYNC/d' "${ROOTFS}"/etc/portage/make.conf
