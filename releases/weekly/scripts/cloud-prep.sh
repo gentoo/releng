e0a2e5d8861040c1709340c0e057e194b31cf94d
Robin H. Johnson <robbat2@gentoo.org>
2016-01-04 14:39:07 -0800

    Move cloud stage cleanup from fsscript to spec where possible.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- releases/weekly/scripts/cloud-prep.sh
+++ releases/weekly/scripts/cloud-prep.sh
@@ -68,13 +68,4 @@ sed -i '/^USE=\"\${USE}\ \ build\"$/d' /etc/portage/make.conf
 # clean up system
 passwd -d root
 passwd -l root
-rm -R -f /usr/portage/distfiles/*
-rm -f /etc/ssh/ssh_host_*
-rm -f /root/.bash_history
-rm -f /root/.nano_history
-rm -f /root/.lesshst
-rm -f /root/.ssh/known_hosts
-rm -rf /usr/src/linux
-rm -rf /usr/src/linux*
-for i in $(find /var/log -type f); do echo > $i; done
-for i in $(find /tmp -type f); do rm -f $i; done
+for i in $(find /var/log -type f); do truncate -s 0 $i; done
