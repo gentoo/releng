4cabd6acedc5f444e4778b6db6a90308ac301db5
Anthony G. Basile <blueness@gentoo.org>
2018-10-05 07:40:04 -0400

    tools-uclibc: update arm build for new 17.0 profiles

--- tools-uclibc/run-armv7a.sh
+++ tools-uclibc/run-armv7a.sh
@@ -13,24 +13,24 @@ prepare_confs() {
     [[ $p == 0 ]] && p=3
     local pstage=stage${p}
     local tarch="${arch%_hardfp}"
-    local parch="arm/${tarch}"
+    local parch="arm"
     local float
 
     [[ "${arch}" == "${tarch}" ]] \
-      && float="softfp" \
-      || float="hardfloat"
+      && float="" \
+      || float="hf"
 
     local profile=${flavor}
-    [[ "${flavor}" == "vanilla" ]] && profile="default"
+    [[ "${flavor}" == "vanilla" ]] && profile="" || profile="/hardened"
 
-    cat stage-all.conf.template | \
+    cat stage.conf.template | \
       sed -e "s:\(^version_stamp.*$\):\1-${mydate}:" \
         -e "s:CSTAGE:${cstage}:g" \
         -e "s:PSTAGE:${pstage}:g" \
         -e "s:SARCH:${arch}:g" \
         -e "s:PARCH:${parch}:g" \
         -e "s:TARCH:${tarch}:g" \
-        -e "s:gentoo-linux-uclibc:${float}-linux-uclibceabi:" \
+        -e "s:gentoo-linux-uclibc:unknown-linux-uclibceabi${float}:" \
         -e "s:FLAVOR:${flavor}:g" \
         -e "s:PROFILE:${profile}:g" \
         -e "s:MYCATALYST:$(pwd):g" \
@@ -46,8 +46,6 @@ prepare_confs() {
 
 
 main() {
-  >zzz.log
-
   catalyst -s current | tee -a zzz.log >snapshot.log 2>snapshot.err
 
 #  for arch in armv7a_hardfp armv7a; do
@@ -57,7 +55,7 @@ main() {
     done
   done
 
-  # No parallelization for arm.  Its too hard on the cpu!
+# No parallelization for arm.  Its too hard on the cpu!
 #  for arch in armv7a_hardfp armv7a; do
   for arch in armv7a_hardfp; do
     for flavor in hardened vanilla; do
