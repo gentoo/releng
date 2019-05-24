96fc8a991b72df2bec314ea15a39c65691612a59
Matthew Thode <mthode@mthode.org>
2019-05-23 22:50:12 -0500

    new musl stage4 uses xz

--- tools-musl/run-stage4.sh
+++ tools-musl/run-stage4.sh
@@ -16,8 +16,8 @@ sed -i "s|MY_DATE|${MY_DATE}|g" "${MUSL_DIR}"/stage4-hardened-amd64-configured.s
 catalyst -f "${MUSL_DIR}"/stage4-hardened-amd64-configured.spec | tee -a "${MUSL_DIR}"/zzz.log
 
 # update link, rm -f returns 0 if file isn't there yet
-rm -f "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened.tar.bz2"
-ln -s "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened-${MY_DATE}.tar.bz2" "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened.tar.bz2"
+rm -f "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened.tar.xz"
+ln -s "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened-${MY_DATE}.tar.xz" "${storedir}/builds/musl/hardened/amd64/stage4-amd64-musl-hardened.tar.xz"
 
 # remove old specfile
 rm "${MUSL_DIR}"/stage4-hardened-amd64-configured.spec
