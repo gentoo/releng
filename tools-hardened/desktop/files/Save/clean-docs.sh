f3a6d7dac5e1ab918885573148eefc528d4e7a1c
Anthony G. Basile <blueness@gentoo.org>
2020-12-19 15:33:19 -0500

    tools-hardened: desktop scripts migrated to GRS
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

--- tools-hardened/desktop/files/Save/clean-docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf /usr/share/doc/*
-rm -rf /usr/share/gnome/help/*
-rm -rf /usr/share/gtk-doc/*
-
--- tools-hardened/desktop/rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-kernel_dir="/usr/src/linux-tinhat"
-
-source /etc/profile
-env-update
-KERNEL_DIR="${kernel_dir}" emerge -evq --keep-going --with-bdeps=y world
