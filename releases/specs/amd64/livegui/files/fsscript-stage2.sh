8254e3b8f0f3a5a020f40d741a1d5e58283f3e4c
Ben Kohler <bkohler@gentoo.org>
2022-02-15 06:20:26 -0600

    amd64/livegui/fsscript: user & session setup
    Autologin to plasma, disable screen autolock, set firefox as default
    browser
    
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- releases/specs/amd64/livegui/files/fsscript-stage2.sh
+++ releases/specs/amd64/livegui/files/fsscript-stage2.sh
@@ -8,3 +8,25 @@ source /tmp/envscript
 
 # No we don't want to run xdm...
 sed -e '/^DISPLAYMANAGER=/s/.*/DISPLAYMANAGER="sddm"/' -i /etc/conf.d/display-manager
+
+# Autologin via sddm to plasma
+echo "[Autologin]
+User=gentoo
+Session=plasma.desktop
+Relogin=yes" > /etc/sddm.conf
+
+# set up gentoo user
+echo "** useradd gentoo **"
+useradd -G users,wheel,audio,plugdev,games,cdrom -g users -c "Gentoo LiveGUI user" -m gentoo
+mkdir /home/gentoo/.config
+
+# Disable screen lock
+echo "[Daemon]
+Autolock=false" > /home/gentoo/.config/kscreenlockerrc
+
+# Firefox as default browser
+echo "[Default Applications]
+text/html=firefox.desktop" > /home/gentoo/.config/mimeapps.list
+
+# Clean up perms
+chown -R gentoo:users /home/gentoo
