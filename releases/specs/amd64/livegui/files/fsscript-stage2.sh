7bfeec8de2e56ab3646786c776343c381c902183
Ben Kohler <bkohler@gentoo.org>
2022-02-18 14:36:12 -0600

    livegui/fsscript: NM fix, taskbar pinned apps
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- releases/specs/amd64/livegui/files/fsscript-stage2.sh
+++ releases/specs/amd64/livegui/files/fsscript-stage2.sh
@@ -9,24 +9,39 @@ source /tmp/envscript
 # No we don't want to run xdm...
 sed -e '/^DISPLAYMANAGER=/s/.*/DISPLAYMANAGER="sddm"/' -i /etc/conf.d/display-manager
 
+# Don't let NM change hostname (this breaks xauth)
+echo "[main]
+plugins=keyfile 
+hostname-mode=none" > /etc/NetworkManager/NetworkManager.conf
+
 # Autologin via sddm to plasma
 echo "[Autologin]
 User=gentoo
 Session=plasma.desktop
 Relogin=yes" > /etc/sddm.conf
 
-# set up gentoo user
-echo "** useradd gentoo **"
-useradd -G users,wheel,audio,plugdev,games,cdrom -g users -c "Gentoo LiveGUI user" -m gentoo
-mkdir /home/gentoo/.config
+# Set up gentoo user
+pushd /home/gentoo
+mkdir -pv .config Desktop
 
 # Disable screen lock
 echo "[Daemon]
-Autolock=false" > /home/gentoo/.config/kscreenlockerrc
+Autolock=false" > .config/kscreenlockerrc
 
 # Firefox as default browser
 echo "[Default Applications]
-text/html=firefox.desktop" > /home/gentoo/.config/mimeapps.list
+text/html=firefox.desktop" > .config/mimeapps.list
 
+# Customize taskbar pinned apps
+wget "https://dev.gentoo.org/~bkohler/plasma-org.kde.plasma.desktop-appletsrc" -O \
+	.config/plasma-org.kde.plasma.desktop-appletsrc
+
+# Desktop icon setups
+#DESKTOP_APPS=( org.kde.konsole firefox org.kde.dolphin )
+#for i in "${APPS[@]}"; do
+#	ln -sv /usr/share/applications/${i}.desktop Desktop/
+#done
+
+popd
 # Clean up perms
 chown -R gentoo:users /home/gentoo
